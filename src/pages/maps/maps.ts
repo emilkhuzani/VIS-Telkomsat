7e977b8c2bb2185729c9454b8674d83700027fd4
Emil Khuzani <emilkhuzani@My-MacBook-Pro.local>
2018-12-06 15:58:00 +0700

    penambahan halaman peta

--- src/pages/maps/maps.ts
+++ src/pages/maps/maps.ts
@@ -3,7 +3,7 @@ import { IonicPage, NavController, NavParams, ActionSheetController, ModalContro
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/timeout';
-declare var google;
+declare var google, MarkerClusterer;
 @IonicPage()
 @Component({
   selector: 'page-maps',
@@ -41,71 +41,33 @@ export class MapsPage {
 
   getPosition(){
   	let marker;
-  	this.http.get('http://vts.patrakom.co.id/api.new/marker/marker.php?id_user='+this.id_user)
+  	this.http.get('http://vis.telkomsat.co.id/api.vessel.tracking/vessel/marker_v3_second.php?id_user='+this.id_user)
   	.map(res=>res.json())
   	.subscribe(data=>{
   	  this.locations = data.locations;
-      Object.keys(this.locations).forEach(key=> {
-      	marker = new google.maps.Marker({
-	        position: new google.maps.LatLng(this.locations[key].latitude,this.locations[key].longitude),
+  	  for(let i=0;i<this.locations.length;i++){
+  	  	marker = new google.maps.Marker({
+	        position: new google.maps.LatLng(this.locations[i].lat,this.locations[i].lng),
 	        map: this.map,
         });
-        let direction = new google.maps.LatLng(this.locations[key].latitude, this.locations[key].longitude);
-        let station = new google.maps.LatLng(this.locations[key].latitude_heading, this.locations[key].longitude_heading);
+        let direction = new google.maps.LatLng(this.locations[i].lat, this.locations[i].lng);
+        let station = new google.maps.LatLng(this.locations[i].latheading, this.locations[i].longheading);
         let heading = google.maps.geometry.spherical.computeHeading(direction,station)+180;
         marker.setIcon({
           fillOpacity: 1,
           strokeWeight: 2,
-          strokeColor : this.locations[key].stroke_color,
-          fillColor: this.locations[key].fill_color,
+          strokeColor : this.locations[i].stroke_color,
+          fillColor: this.locations[i].fill_color,
           path: "M70 125 L 70 245 180 245 180 125 Q 180 65 125 10 70 70 70 125 M 70 185 L 180 185",
           scale: 0.060,
           rotation: heading
         });
-        let nama=this.locations[key].nama_remote;
-        let id_kapal=this.locations[key].id_remote;
         google.maps.event.addListener(marker, 'click', () =>{
-          let menuSheet = this.actionSheet.create({
-            title: nama,
-            cssClass : 'nama-kapal',
-            buttons:[
-              {
-                text : 'Close',
-                role : 'cancel',
-                cssClass : 'mybutton',
-                icon : 'ios-close-circle-outline',
-                handler:()=>{
-                  console.log('close');
-                }
-              },
-              {
-                text : 'Detail',
-                icon : 'ios-help-buoy-outline',
-                cssClass : 'mybutton',
-                handler :()=>{
-                  let modalDetail = this.modalCtlr.create(DetailPage, { nama_kapal : nama, id_kapal : id_kapal });
-                  modalDetail.present();
-                }
-
-              },
-              {
-              	text : 'Tracking',
-                icon :  'ios-clock-outline',
-                cssClass : 'mybutton',
-              	handler:()=>{
-              	  let modalTracking = this.modalCtlr.create(ModalInputTrackingPage, { nama_kapal : nama, id_kapal :  id_kapal });
-                  modalTracking.present();
-              	}
-              },
-              
-            ]
-          });
-          menuSheet.present();
+          
         });
-        
         this.markers.push(marker);
-        console.log(this.markers);
-	  });
+  	  }
+  	  var markerCluster = new MarkerClusterer(this.map, this.markers,{imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
   	});
   	
   }
